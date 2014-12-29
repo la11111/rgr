3ec4675abfa2713730728b6ed68106757cd12aad
Brenna D <brenna@trans.rocks>
2014-12-29 00:59:11 -0600

    added setup.py and stuffs

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(name='rgr',
+    version='0.1',
+    description='a(nother) graph database built on Redis',
+    author='Brenna D',
+    author_email='linuxaddict11111@gmail.com',
+    url='https://github.com/la11111/rgr',
+    py_modules=['rgr'],
+    )
